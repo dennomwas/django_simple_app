bbdde91875fae228f0fb624ab6af63337bd6e719
dennomwas <githinji.mwangi@gmail.com>
2018-07-16 23:29:34 +0300

    [Feat] Add Login urls
    - Add the login view

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import include
 
 from .import views
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
